4246f8829f2d00cf22b476c38a3c87882d08b84d
Nikolai Krawielitzki <nikolai.krawielitzki@oak-engineering.de>
2026-07-24 07:55:47 +0200

    Feld-Tools aktualisiert: Begehungsbogen + Maschinensicherheit

--- begehung/sw.js
+++ begehung/sw.js
@@ -10,7 +10,7 @@
 
    WICHTIG: Bei JEDER Aenderung am Tool die Cache-Version hochzaehlen (v2 -> v3 ...), damit
    activate den alten Cache purged. */
-const CACHE = "oak-begehung-v2";
+const CACHE = "oak-begehung-v3";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", e => {
